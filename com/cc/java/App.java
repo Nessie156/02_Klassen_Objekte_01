3fd9506cfc7032c044154c4b587598c3f1680535
Peter <schmidt.multimedia@gmail.com>
2023-04-18 12:29:03 +0200

    01 / Instanziierung Klasse Cat

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,6 +4,12 @@ public class App {
     
     public static void main(String[] args) {
 
+        /* Instanziierung Klasse --> Objekt */
+        // Cat -- Referenztyp
+        // cat -- Referenzvariable
+        
+        Cat cat = new Cat();
+        System.out.println(cat);
 
     }
 
